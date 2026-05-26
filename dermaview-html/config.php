4198df374f13e70d99be505e05fc01d5d62643b7
amcfajardo <amcfajardo@mymail.mapua.edu.ph>
2026-05-26 21:28:08 +0800

    s

--- dermaview-html/config.php
+++ dermaview-html/config.php
@@ -1,32 +1,14 @@
 <?php
 
 $host = "localhost";
-$user = "dermaviewuser";
+$username = "dermaviewuser";
 $password = "admin123";
 $database = "dermaview";
 
-mysqli_report(MYSQLI_REPORT_OFF);
-$conn = @new mysqli($host, $user, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database);
 
-if ($conn->connect_error) {
-    http_response_code(503);
-
-    $message = "The clinic database is currently unavailable. Please try again in a moment.";
-    $accept = $_SERVER['HTTP_ACCEPT'] ?? '';
-    $action = $_POST['action'] ?? '';
-
-    if (stripos($accept, 'application/json') !== false || str_ends_with((string) $action, '_json')) {
-        header('Content-Type: application/json; charset=utf-8');
-        echo json_encode([
-            'status' => 'error',
-            'message' => $message
-        ]);
-    } else {
-        header('Content-Type: text/plain; charset=utf-8');
-        echo $message;
-    }
-
-    exit();
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
 }
 
 ?>
